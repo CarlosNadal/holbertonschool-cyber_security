60fb6d3a43fffe4c8aeb7092279474b2e3f396f1
SrN <91611215+CarlosNadal@users.noreply.github.com>
2024-11-18 20:53:07 -0300

    Update 0-attack_ip.sh

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-# Check if a log file is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <logfile>"
-  exit 1
-fi
-
-# Assign log file to a variable
-LOG_FILE="$1"
-
-# Extract IP addresses from the log file, count occurrences, and sort to find the most frequent one
-MOST_REQUESTED_IP=$(awk '{print $1}' $LOG_FILE | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
-
-# Print the result
-echo "$MOST_REQUESTED_IP"
-
+awk '{print $1}' $1 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
