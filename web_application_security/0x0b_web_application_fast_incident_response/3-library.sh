7b69397e76d603e0025e093e41b55088945da2f4
SrN <91611215+CarlosNadal@users.noreply.github.com>
2024-11-19 12:06:30 -0300

    Update 3-library.sh

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat logs.txt | grep "python" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $13}'
+grep "python" logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $13}'
