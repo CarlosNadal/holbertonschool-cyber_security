29a7725abcd2c4c23c9afd9adace87636460b88d
SrN <91611215+CarlosNadal@users.noreply.github.com>
2024-11-19 11:20:40 -0300

    Update 0-attack_ip.sh

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -e [^0-9] $1 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' 
+grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' 
