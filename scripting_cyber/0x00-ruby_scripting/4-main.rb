582869d169943b93123df1c5833102298153255a
carlos <carlitosnadalfiuri05@gmail.com>
2024-12-13 17:57:16 -0500

    first commit

--- scripting_cyber/0x00-ruby_scripting/4-main.rb
+++ scripting_cyber/0x00-ruby_scripting/4-main.rb
@@ -6,10 +6,11 @@ require_relative '4-write_file'
 file1_path = 'file_to_copy_from.json'
 file2_path = 'file.json'
 
+puts 'Merged JSON written to file.json'
 
 merge_json_files(file1_path, file2_path)
 
-puts 'Merged JSON written to file.json'
+
 puts 'File merged successfully!'
 
 count_user_ids(file2_path)
